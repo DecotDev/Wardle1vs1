91c98e1d01d943f768df1a997126d5b47ffe649d
dam2a <bperez@elpuig.xeill.net>
2025-01-15 18:34:56 +0100

    Separado

--- Game.java
+++ Game.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class Game {
+    Scanner sc = new Scanner(System.in);
+
+    public void jugar(Cliente a){
+    boolean correcto = false;
+        System.out.println("Welcome to WARdle.");
+        System.out.println("The word has "+a.palabra.length()+" letters. Good luck!");
+
+    int turnos = 0;
+        while (!correcto){
+        turnos++;
+        System.out.println("Turn:\t"+ turnos+ "\nTry to guess the word:");
+        String guess = sc.nextLine();
+
+        if (guess.length()!=a.palabra.length()){
+            System.out.println("The word has "+a.palabra.length()+" letters. Try again");
+            continue;
+        }
+
+        correcto = a.victoria(guess);
+        System.out.println(a.respuesta);
+    }
+    }
 }
--- Main.java
+++ Main.java
@@ -3,25 +3,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Cliente a = new Cliente("huevo");
-        boolean correcto = false;
-        System.out.println("Welcome to WARdle.");
-        System.out.println("The word has "+a.palabra.length()+" letters. Good luck!");
 
-        int turnos = 0;
-        while (!correcto){
-            turnos++;
-            System.out.println("Turn:\t"+ turnos+ "\nTry to guess the word:");
-            String guess = sc.nextLine();
-
-            if (guess.length()!=a.palabra.length()){
-                System.out.println("The word has "+a.palabra.length()+" letters. Try again");
-                continue;
-            }
-
-            correcto = a.victoria(guess);
-            System.out.println(a.respuesta);
-        }
+        Cliente a = new Cliente("a");
+        Game b = new Game();
+        b.jugar(a);
     }
 }
