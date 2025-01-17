e9e1e3652ca4392934f0df95bde941db4084299a
DecotDev <decot013@gmail.com>
2025-01-17 17:58:24 +0100

    What's new? - Server class

---
+++ src/ServerWardle.java
@@ -0,0 +1,20 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerWardle {
+
+    private int port;
+
+    public ServerWardle(int port) {
+        this.port = port;
+    }
+
+    private void listen () {
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
